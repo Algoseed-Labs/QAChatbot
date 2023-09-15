83a79a7039a2c80ee99bafcf60368863e3e8e779
Niko <37193485+xenosnikos@users.noreply.github.com>
2023-09-15 22:29:26 +0300

    Update ask_question.py

--- ask_question.py
+++ ask_question.py
@@ -6,15 +6,15 @@ import pickle
 from langchain import OpenAI
 from langchain.chains import VectorDBQAWithSourcesChain
 
-parser = argparse.ArgumentParser(description='Paepper.com Q&A')
-parser.add_argument('question', type=str, help='Your question for Paepper.com')
+parser = argparse.ArgumentParser(description='Website Q&A')
+parser.add_argument('question', type=str, help='Your question for the inputed site')
 args = parser.parse_args()
 
 with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
 chain = VectorDBQAWithSourcesChain.from_llm(
-        llm=OpenAI(temperature=0, verbose=True), vectorstore=store, verbose=True)
+        llm=OpenAI(temperature=0.5, verbose=True), vectorstore=store, verbose=True)
 result = chain({"question": args.question})
 
 print(f"Answer: {result['answer']}")
